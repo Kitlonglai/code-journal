c79512cc7ea57b8341d8a32688f7b54b34b923bd
Kitlong Lai <thekitlong@gmail.com>
2023-06-02 03:20:45 +0000

    getting values of form inputs inside local storage and resetting the form after #1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 const photoInput = document.querySelector('#photourl');
 const form = document.querySelector('form');
 const imageDisplay = document.querySelector('.display');
-
+const img = document.querySelector('img');
 photoInput.addEventListener('input', () => {
   imageDisplay.setAttribute('src', photoInput.value);
 });
 
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  const formInput = {};
-  formInput.title = document.querySelector('#title').value;
-  formInput.photoUrl = document.querySelector('#photourl').value;
-  formInput.notes = document.querySelector('#notes').value;
-  formInput.entryId = data.nextEntryId;
+  const formInput = {
+    title: event.target.value,
+    photoUrl: event.target.value,
+    notes: event.target.value,
+    entryId: data.nextEntryId
+  };
   data.nextEntryId++;
   data.entries.unshift(formInput);
-  photoInput.setAttribute('src', 'default');
+  img.setAttribute('src', 'images/placeholder-image-square.jpg');
   form.reset();
 });
